98317a18f1e406ef4843957ec9ed98ec5f44a552
Dipta Dey <57134360+dipta995@users.noreply.github.com>
2023-02-13 22:38:58 +0600

    Db Up

--- online_edu.sql
+++ online_edu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2022 at 07:30 PM
+-- Generation Time: Feb 13, 2023 at 05:37 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -165,7 +165,8 @@ INSERT INTO `course_table` (`course_id`, `course_title`, `course_details`, `cate
 (16, 'Digital Marketing ', 'Digital marketing is the component of marketing that utilizes internet and online based digital technologies such as desktop computers, mobile phones and other digital media and platforms to promote products and services.[1][2] Its development during the 1990s and 2000s, changed the way brands and businesses use technology for marketing. As digital platforms became increasingly incorporated into marketing plans and everyday life,[3] and as people increasingly use digital devices instead of visiting physical shops,[4][5] digital marketing campaigns have become prevalent, employing combinations of search engine optimization (SEO), search engine marketing (SEM), content marketing, influencer marketing, content automation, campaign marketing, data-driven marketing, e-commerce marketing, social media marketing, social media optimization, e-mail direct marketing, display advertising, e–books, and optical disks and games have become commonplace. Digital marketing extends to non-Internet channels that provide digital media, such as television, mobile phones (SMS and MMS), callback, and on-hold mobile ring tones.[6] The extension to non-Internet channels differentiates digital marketing from online ', 4, 9, '5000', 'img/DIGITAL-MARKETING.jpg', 0, 0, 5, 1),
 (17, 'Affilate marketing', 'Wake up at an ungodly hour. Drive to the office through total gridlock, streets jammed with other half-asleep commuters. Slog through email after mind-numbing email until the sweet release at five o’clock.\r\n\r\nSound terrible?\r\n\r\nWhat if, instead of dealing with the monotony and stupor of the rat race to earn a few bucks, you could make money at any time, from anywhere — even while you sleep?\r\n\r\nThat’s the concept behind affiliate marketing.', 4, 9, '2000', 'img/MG-Blog-8.png', 0, 0, 0, 0),
 (18, 'logo design', 'about logo design এর ছবির ফলাফল\r\nLogo design is a branding and marketing tool that can be used to signify a business. Put simply; your logo represents your business brand. ... We see logo design every day, most logo design is used in a business context, or to represent a business. A logo design is used as a signifier.', 5, 11, '10000', 'img/8df2cfa314f6704ca1ee0d9c42361e01.jpg', 0, 0, 5, 1),
-(19, 'fsdfn fjaf ', 'dnsfss jdslf', 3, 4, '', 'img/308929843_126710550145926_2290180168558119142_n.jpg', 0, 0, 0, 0);
+(19, 'fsdfn fjaf ', 'dnsfss jdslf', 3, 4, '', 'img/308929843_126710550145926_2290180168558119142_n.jpg', 0, 0, 0, 0),
+(20, 'Optio consectetur ', 'Commodo minus eiusmo', 1, 4, '500', 'img/75px-Laravel.svg.png', 0, 0, 5, 1);
 
 -- --------------------------------------------------------
 
@@ -179,154 +180,36 @@ CREATE TABLE `enrole_student_list` (
   `course_id` int(11) NOT NULL,
   `price` varchar(255) NOT NULL,
   `pay_status` tinyint(4) NOT NULL,
+  `student_account` varchar(191) DEFAULT NULL,
+  `student_payment_type` varchar(191) NOT NULL,
   `teacher_pay` decimal(30,2) NOT NULL,
-  `t_pay` tinyint(4) NOT NULL
+  `t_pay` tinyint(4) NOT NULL,
+  `teacher_account` varchar(191) DEFAULT NULL,
+  `teacher_payment_type` varchar(191) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `enrole_student_list`
 --
 
-INSERT INTO `enrole_student_list` (`enrole_id`, `student_id`, `course_id`, `price`, `pay_status`, `teacher_pay`, `t_pay`) VALUES
-(26, 1, 1, '2500', 1, '0.90', 1),
-(28, 10, 2, '5555', 1, '4999.50', 1),
-(29, 10, 15, '10000', 1, '9000.00', 1),
-(30, 10, 3, '0', 1, '0.90', 1),
-(32, 11, 18, '10000', 1, '9000.00', 1);
+INSERT INTO `enrole_student_list` (`enrole_id`, `student_id`, `course_id`, `price`, `pay_status`, `student_account`, `student_payment_type`, `teacher_pay`, `t_pay`, `teacher_account`, `teacher_payment_type`) VALUES
+(39, 1, 2, '5555', 1, '234578', 'Nogod', '4444.00', 1, '545454', 'Bkash'),
+(40, 1, 3, '0', 1, '01767654345', 'Bkash', '0.00', 0, '545454', 'Bkash'),
+(41, 1, 13, '7000', 1, '45678909654', 'Nogod', '5600.00', 1, '123456789', 'Nogod'),
+(42, 1, 16, '5000', 1, '01767654345', 'Bkash', '4000.00', 1, '2147483647', 'Bkash');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `quiz_ans_table`
+-- Table structure for table `pdf_table`
 --
 
-CREATE TABLE `quiz_ans_table` (
-  `ans_id` int(11) NOT NULL,
-  `answer` tinyint(4) NOT NULL,
-  `multiple` text NOT NULL,
-  `quiz_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `quiz_ans_table`
---
-
-INSERT INTO `quiz_ans_table` (`ans_id`, `answer`, `multiple`, `quiz_id`) VALUES
-(130, 0, 'Playing a game on Computer', 26),
-(131, 1, 'Making a machine Intelligent', 26),
-(132, 0, 'Programming on Machine with your Own Intelligence', 26),
-(133, 0, 'Putting your intelligence in Machine', 26),
-(142, 0, 'Playing a game on Computer', 28),
-(143, 1, 'Making a machine Intelligent', 28),
-(144, 0, 'Programming on Machine with your Own Intellig', 28),
-(145, 0, 'Putting your intelligence in Machine', 28),
-(154, 0, 'Real-life situation', 31),
-(155, 1, 'Small Search Space', 31),
-(156, 0, 'Complex game', 31),
-(157, 0, 'All of the above', 31),
-(158, 0, 'Fisher Ada', 32),
-(159, 0, 'Alan Turing', 32),
-(160, 1, 'John McCarthy', 32),
-(161, 0, 'Allen Newell', 32),
-(162, 0, 'Optimal Search', 33),
-(163, 1, 'Depth First Search', 33),
-(164, 0, 'Breadth-First Search', 33),
-(165, 0, 'Linear Search', 33),
-(166, 0, 'The whole problem', 34),
-(167, 0, ' Your Definition to a problem', 34),
-(168, 0, 'Problem you design', 34),
-(169, 1, 'Representing your problem with variable and p', 34),
-(170, 0, 'The whole problem', 35),
-(171, 0, 'Your Definition to a problem', 35),
-(172, 0, 'Problem you design', 35),
-(173, 1, 'Representing your problem with variable and p', 35),
-(174, 1, ' Initial state', 36),
-(175, 0, 'Last state', 36),
-(176, 0, 'Intermediate state', 36),
-(177, 0, 'All of the mentioned', 36),
-(178, 0, 'Intermediate states', 37),
-(179, 0, 'Initial state', 37),
-(180, 1, 'Successor function, which takes current action and returns next immediate stat', 37),
-(181, 0, 'None of the mentioned', 37),
-(182, 0, 'Extraction', 38),
-(183, 1, 'Abstraction', 38),
-(184, 0, 'Information Retrieval', 38),
-(185, 0, 'Mining of data', 38),
-(186, 0, '8-Puzzle problem', 39),
-(187, 0, '8-queen problem', 39),
-(188, 0, 'Finding a optimal path from a given source to a destination', 39),
-(189, 1, 'Mars Hover (Robot Navigation)', 39),
-(190, 0, 'nothing', 40),
-(191, 0, 'something', 40),
-(192, 0, 'anything', 40),
-(193, 1, 'design', 40),
-(194, 0, ' Meow ', 41),
-(195, 0, 'Hoo', 41),
-(196, 1, 'Oink ', 41),
-(197, 0, 'Mooo ', 41),
-(198, 1, ' Star Wars ', 42),
-(199, 0, 'Lion King Shrek', 42),
-(200, 0, 'Caribbean Toy Story ', 42),
-(201, 0, 'Pirates Of The ', 42);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `quiz_result_table`
---
-
-CREATE TABLE `quiz_result_table` (
-  `result_id` int(11) NOT NULL,
-  `quiz_id` int(11) NOT NULL,
-  `course_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `student_ans` int(11) NOT NULL,
-  `given_ans` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `quiz_result_table`
---
-
-INSERT INTO `quiz_result_table` (`result_id`, `quiz_id`, `course_id`, `student_id`, `student_ans`, `given_ans`) VALUES
-(22, 37, 15, 10, 179, 180),
-(23, 40, 18, 11, 190, 193),
-(24, 41, 18, 11, 196, 196),
-(25, 42, 18, 11, 198, 198),
-(26, 38, 16, 1, 184, 183);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `quiz_table`
---
-
-CREATE TABLE `quiz_table` (
-  `quiz_id` int(11) NOT NULL,
-  `quiz_flag` tinyint(11) NOT NULL,
-  `quiz_question` text NOT NULL,
+CREATE TABLE `pdf_table` (
+  `pdf_id` int(11) NOT NULL,
+  `pdf_title` varchar(300) NOT NULL,
+  `pdf_file` varchar(300) NOT NULL,
   `course_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `quiz_table`
---
-
-INSERT INTO `quiz_table` (`quiz_id`, `quiz_flag`, `quiz_question`, `course_id`) VALUES
-(26, 1, 'Artificial Intelligence is about_____.', 13),
-(28, 1, 'Artificial Intelligence is about_____.', 3),
-(31, 1, 'Who is known as the -Father of AI\"?', 13),
-(32, 1, 'Select the most appropriate situation for that a blind search can be used.', 13),
-(33, 1, 'Among the given options, which search algorithm requires less memory?', 13),
-(34, 1, 'What is state space?', 13),
-(35, 1, 'What is state space?', 14),
-(36, 1, 'A problem in a search space is defined by one o', 14),
-(37, 1, 'The Set of actions for a problem in a state space is formulated by a ___________', 15),
-(38, 1, 'The process of removing detail from a given state representation is called ______', 16),
-(39, 1, 'A problem solving approach works well for ______________', 17),
-(40, 1, 'What is logo desing ?', 18),
-(41, 1, 'This is an example multiple choice question. What sound does a dog make?', 18),
-(42, 1, 'This is an example of using vidoes in your questions. What Movie Series Are These Songs From?', 18);
 
 -- --------------------------------------------------------
 
@@ -339,18 +222,6 @@ CREATE TABLE `ratecheck_table` (
   `course_id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `ratecheck_table`
---
-
-INSERT INTO `ratecheck_table` (`ratcheck_id`, `course_id`, `student_id`) VALUES
-(9, 14, 8),
-(10, 1, 1),
-(11, 18, 11),
-(12, 16, 1),
-(13, 15, 10),
-(14, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -424,7 +295,7 @@ CREATE TABLE `teacher_table` (
   `teacher_image` varchar(266) NOT NULL,
   `teacher_password` varchar(266) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `account_no` int(11) NOT NULL
+  `account_no` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -438,8 +309,7 @@ INSERT INTO `teacher_table` (`teacher_id`, `teacher_name`, `teacher_email`, `tea
 (9, 'teacher two', 'teacher2@gmail.com', '123456789962', 'img/maxresdefault.jpg', '12', '2021-02-26 09:00:03', 2147483647),
 (10, 'TEACHER THREE', 'teacher3@gmail.com', '0161234568', 'img/smart-person-eyewear-man-official-clothes-stands-against-white-background-studio-165962936.jpg', '12', '2021-02-26 09:02:15', 123456468),
 (11, 'TEACHER FOUR', 'teacher4@gmail.com', '0161234568', 'img/Super_Intelligence_v3.jpg', '12', '2021-02-26 09:02:56', 1215469846),
-(12, 'Me student', 'me@gmail.com', '0161234568', '', '12', '2021-02-26 13:03:04', 1215469846),
-(13, 'dipta', 'dipta995@gmail.com', '12345678909', '', '1234567890', '2022-03-25 06:57:45', 1215469846);
+(23, 'dipta', 'stufdssddent@gmail.com', '01654543456', '', '123452345', '2023-02-06 17:49:32', 123);
 
 -- --------------------------------------------------------
 
@@ -479,7 +349,10 @@ INSERT INTO `video_table` (`video_id`, `video_title`, `video_file`, `course_id`)
 (23, 'a 2 ', 'video/yt1s.com - Java Bangla Tutorials 15 Temperature Converter_v144P.mp4', 17),
 (24, 'a 3', 'video/yt1s.com - Java Bangla Tutorials 17 Relational Operator_v144P.mp4', 17),
 (25, 'video 1 ', 'video/yt1s.com - Java Bangla Tutorials 17 Relational Operator_v144P.mp4', 18),
-(26, 'video 2', 'video/yt1s.com - Java Bangla Tutorials 23 switch part1_v144P.mp4', 18);
+(26, 'video 2', 'video/yt1s.com - Java Bangla Tutorials 23 switch part1_v144P.mp4', 18),
+(27, 'jfl', 'video/videoplayback (1).mp4', 20),
+(28, '', 'video/yt1s.com - Java Bangla Tutorials 23 switch part1_v144P.mp4', 1),
+(29, 'dfgh', 'video/yt1s.com - Java Bangla Tutorials 23 set up jdk with netbeans together jdk not found solution_v144P.mp4', 1);
 
 --
 -- Indexes for dumped tables
@@ -522,22 +395,10 @@ ALTER TABLE `enrole_student_list`
   ADD PRIMARY KEY (`enrole_id`);
 
 --
--- Indexes for table `quiz_ans_table`
+-- Indexes for table `pdf_table`
 --
-ALTER TABLE `quiz_ans_table`
-  ADD PRIMARY KEY (`ans_id`);
-
---
--- Indexes for table `quiz_result_table`
---
-ALTER TABLE `quiz_result_table`
-  ADD PRIMARY KEY (`result_id`);
-
---
--- Indexes for table `quiz_table`
---
-ALTER TABLE `quiz_table`
-  ADD PRIMARY KEY (`quiz_id`);
+ALTER TABLE `pdf_table`
+  ADD PRIMARY KEY (`pdf_id`);
 
 --
 -- Indexes for table `ratecheck_table`
@@ -601,37 +462,25 @@ ALTER TABLE `comment_table`
 -- AUTO_INCREMENT for table `course_table`
 --
 ALTER TABLE `course_table`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `enrole_student_list`
 --
 ALTER TABLE `enrole_student_list`
-  MODIFY `enrole_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `enrole_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
--- AUTO_INCREMENT for table `quiz_ans_table`
+-- AUTO_INCREMENT for table `pdf_table`
 --
-ALTER TABLE `quiz_ans_table`
-  MODIFY `ans_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202;
-
---
--- AUTO_INCREMENT for table `quiz_result_table`
---
-ALTER TABLE `quiz_result_table`
-  MODIFY `result_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
---
--- AUTO_INCREMENT for table `quiz_table`
---
-ALTER TABLE `quiz_table`
-  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+ALTER TABLE `pdf_table`
+  MODIFY `pdf_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `ratecheck_table`
 --
 ALTER TABLE `ratecheck_table`
-  MODIFY `ratcheck_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ratcheck_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `replay_table`
@@ -649,13 +498,13 @@ ALTER TABLE `student_table`
 -- AUTO_INCREMENT for table `teacher_table`
 --
 ALTER TABLE `teacher_table`
-  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `video_table`
 --
 ALTER TABLE `video_table`
-  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
